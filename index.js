03854289c9da29b1798959b9d5b6578c606486d4
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-04-25 11:37:34 +0300

    refactor code

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ new Vue({
     data: {
         dividend: 0,
         divider: 0,
-        answer: "",
+        quotient: 0,
+        answer: '',
         failed: false,
         wrongAnswer: false,
         counter: 0
     },
     methods: {
         handleAnswerBtnClick(answer) {
-            if ((answer === this.dividend / this.divider) && !this.failed) {
+            if ((answer === this.quotient) && !this.failed) {
                 this.counter += 1;
                 this.generateNewExpression();
             } else {
-                this.turnOnWrongAnswer();
+                this.highlightWrongAnswer();
                 this.counter = 0;
             }
         },
@@ -27,15 +28,16 @@ new Vue({
             }
         },
         generateNewExpression() {
-            this.divider = this.getRandomInt();
-            this.dividend = this.divider * this.getRandomInt();
+            this.divider = this.getRandom(10);
+            this.quotient = this.getRandom(10);
+            this.dividend = this.divider * this.quotient;
             this.answer = "?";
             this.failed = false;
         },
-        getRandomInt() {
-            return Math.floor(Math.random() * 10) + 1;
+        getRandom(max) {
+            return Math.floor(Math.random() * max) + 1;
         },
-        turnOnWrongAnswer() {
+        highlightWrongAnswer() {
             const instance = this;
             instance.wrongAnswer = true;
             setTimeout(() => (instance.wrongAnswer = false), 500);
